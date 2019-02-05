69cfafbd58b5ad2056093e9c3bf99dbcdd5a3adb
Adeyefa Oluwatoba <sainttobs@gmail.com>
2019-02-05 11:05:40 +0100

    finding sum of numbers below an integer using recursion

--- recursion.js
+++ recursion.js
@@ -12,4 +12,18 @@ function factorial(n) { //A javascript function to calculate the factorial of a 
 	}
 }
 
+// A simple program to find the sum of all numbers below an integer using recursion
+
 console.log(factorial(4)); //24
+
+function sum(n){
+	if (n===0) {
+		//console.log("We hit the case");
+		return 0;
+	}
+	else{
+		return n + sum(n-1);
+	}
+}
+
+console.log(sum(10));
